7f2e356262f66fc25c8d3b44a0f683fc0f24bff6
Gabriel Holmkvist <gabbeholman99@gmail.com>
2021-04-13 11:51:18 +0200

    added highscores.json and fetch

--- src/components/gameDetails/gameDetails.js
+++ src/components/gameDetails/gameDetails.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
+
+
 const GameDetails = () => {
+
+
+let [highscores, setHighscores] = useState();
+
+
+fetch('../highscores.json').then(response => {
+    return response.json();
+  }).then(data => {
+    setHighscores(data);
+    // console.log(highscores[0].player);  
+    
+  }).catch(err => {
+    
+  });
+
+
 
    return (
      <div>
        <h1>Hello World</h1>
-       <h1>This is a page</h1>
+       <h1>  </h1>
      </div>
    );
  };
